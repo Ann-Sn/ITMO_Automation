9df7ad96ebd9c15001f055daa9e81b8e9f9d099f
Ann-Sn <ansuchova@gmail.com>
2024-08-18 14:29:51 +0300

    Update 3_hw доп.py

--- home_work/3_hw доп.py
+++ home_work/3_hw доп.py
@@ -1,1 +1,13 @@
+def count_positive(numbers):
+    count = 0
+    for num in numbers:
+        if num > 0:
+            count += 1
+    return count
 
+numbers = [int(input()) for _ in range(5)]  
+print(count_positive(numbers))  
+
+
+def days_count(years, months):
+    return years * 365 + months * 29
