dc349b14784a14486dde08165276ff4fe2d5c8c4
Ann-Sn <ansuchova@gmail.com>
2024-08-18 14:26:10 +0300

    Update 3_hw.py

--- home_work/3_hw.py
+++ home_work/3_hw.py
@@ -23,3 +23,30 @@ result = check_difference(num1, num2)
 print(result)
 
 
+def get_season(month):
+    if month == 12 or month == 1 or month == 2:
+        return "зима"
+    elif month >= 3 and month <= 5:
+        return "весна"
+    elif month >= 6 and month <= 8:
+        return "лето"
+    else:
+        return "осень"
+
+month = int(input("Введите номер месяца (от 1 до 12): "))
+print(get_season(month))
+```
+
+def check_numbers(num1, num2, num3):
+if num1 > 10 and num2 > 10 and num3 > 10:
+return "yes"
+else:
+return "no"
+
+num1 = int(input("Введите первое число: "))
+num2 = int(input("Введите второе число: "))
+num3 = int(input("Введите третье число: "))
+
+print(check_numbers(num1, num2, num3))
+
+
