16d19cbd4bd7a912a480ea10626c8050c2009582
Ann-Sn <ansuchova@gmail.com>
2024-08-18 14:20:08 +0300

    Update 3_hw.py

--- home_work/3_hw.py
+++ home_work/3_hw.py
@@ -8,3 +8,18 @@ num1 = float(input("Введите первое число: "))
 num2 = float(input("Введите второе число: "))
 
 print(max_num(num1, num2))
+
+
+def check_difference(num1, num2):
+    if (num1 - num2) == 135 or (num2 - num1) == 135:
+        return "yes"
+    else:
+        return "No"
+
+num1 = int(input("Введите первое число: "))
+num2 = int(input("Введите второе число: "))
+
+result = check_difference(num1, num2)
+print(result)
+
+
