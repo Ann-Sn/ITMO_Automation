57490cdcabe9f7da5d67fe2221ca977f49eba5cf
Ann-Sn <ansuchova@gmail.com>
2024-08-18 14:11:08 +0300

    Update 3_hw.py

--- home_work/3_hw.py
+++ home_work/3_hw.py
@@ -1,1 +1,10 @@
+def max_num(a, b):
+if a > b:
+return a
+else:
+return b
 
+num1 = float(input("Введите первое число: "))
+num2 = float(input("Введите второе число: "))
+
+print(max_num(num1, num2))
