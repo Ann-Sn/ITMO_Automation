999758ca371899d6faa3a58972e70f8aa778f526
Ann-Sn <ansuchova@gmail.com>
2024-08-31 13:52:12 +0300

    5_hw.py

---
+++ home_work/5_hw.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+
+
+def test_visit():
+    driver = webdriver.Chrome()
+    driver.get("https://www.saucedemo.com/")
+
+    try:
+        driver.find_element(By.XPATH, "//*[@id='user-name']")
+        driver.find_element(By.XPATH, "//*[@id='password']")
+        driver.find_element(By.XPATH, "//*[@id='login-button']")
+    except NoSuchElementException:
+        assert False
+    assert True
+    print("Элементы найдены")
